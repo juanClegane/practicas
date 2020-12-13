a74e3ca7449dd32a7b9d61949f2727b60783d351
clegane <juanmtze1992@gmail.com>
2020-12-13 16:51:29 -0600

    Actualizamos el readmo con todos los archivos a omitir

--- src/main/java/com/mx/TesMain.java
+++ src/main/java/com/mx/TesMain.java
@@ -3,7 +3,10 @@ package com.mx;
 public class TesMain {
 	
 	public static void main(String args[]) {
-		
+		//Debemos omitir estos archivos y carpetas
+		//Node modules 
+		//.log
+		//editor config
 		
 	}
 }
